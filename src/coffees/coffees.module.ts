93775be310165db00d5c20a206924cbd5d2da094
Kamil <buksakowskikamil@gmail.com>
2022-01-31 22:24:47 +0100

    eincapsulation, diving into providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,15 +1,22 @@
-import { Module } from '@nestjs/common';
+import {Injectable, Module} from '@nestjs/common';
 import {CoffeesController} from "./coffees.controller";
 import {CoffeesService} from "./coffees.service";
-import { Event } from '../events/entities/event.entity';
+import {Event} from '../events/entities/event.entity';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Coffee} from "./entities/coffee.entity";
-import { Flavor } from "./entities/flavor.entity";
+import {Flavor} from "./entities/flavor.entity";
+import {COFFEE_BRANDS} from "./coffees.constants";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [CoffeesService]
+    providers: [
+        CoffeesService,
+        {provide: COFFEE_BRANDS,
+            useFactory: ()=> ['buddy brew', 'nescafe']}
+    ],
+    exports: [CoffeesService]
 })
-export class CoffeesModule {}
+export class CoffeesModule {
+}
