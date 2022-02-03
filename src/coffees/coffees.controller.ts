c2d8e7dc5eecf9402f32cf12a6b19b9dcda3f92a
Kamil <buksakowskikamil@gmail.com>
2022-02-03 15:44:53 +0100

    create public.decorator.ts and api-key-guard.ts

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,51 +1,49 @@
 import {
-    Controller,
-    Get,
-    Param,
-    Post,
-    Body,
-    Patch,
-    Delete,
-    Query, Inject, UsePipes, ValidationPipe
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query, Inject, UsePipes, ValidationPipe, SetMetadata
 } from "@nestjs/common";
-import {CoffeesService} from "./coffees.service";
-import {CreateCoffeeDto} from "./dto/create-coffee.dto";
-import {UpdateCoffeeDto} from "./dto/update-coffee.dto";
-import {PaginationQueryDto} from "../common/dto/pagination-query.dto";
+import { CoffeesService } from "./coffees.service";
+import { CreateCoffeeDto } from "./dto/create-coffee.dto";
+import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import { PaginationQueryDto } from "../common/dto/pagination-query.dto";
 import { REQUEST } from "@nestjs/core";
 import { Request } from "express";
+import { Public } from "../common/decorators/public.decorator";
 
-@Controller('coffees')
+@Controller("coffees")
 export class CoffeesController {
-    constructor(private readonly coffeesServices: CoffeesService, @Inject(REQUEST) private readonly request:Request) {
-        console.log('CoffeController instantied')
+  constructor(private readonly coffeesServices: CoffeesService, @Inject(REQUEST) private readonly request: Request) {
+  }
 
-    }
+  @Public()
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesServices.findAll(paginationQuery);
+  }
 
+  @Get(":id")
+  findOne(@Param("id") id: number) {
+    return this.coffeesServices.findOne("" + id);
+  }
 
-    @Get()
-    findAll(@Query() paginationQuery: PaginationQueryDto) {
-        return this.coffeesServices.findAll(paginationQuery)
-    }
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesServices.create(createCoffeeDto);
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id:number){
-        console.log(typeof id)
-        return this.coffeesServices.findOne("" + id)
-    }
+  @Patch(":id")
+  update(@Param("id") id: string, @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesServices.update(id, updateCoffeeDto);
+  }
 
-    @Post()
-    create(@Body() createCoffeeDto: CreateCoffeeDto) {
-        return this.coffeesServices.create(createCoffeeDto)
-    }
-
-    @Patch(':id')
-    update(@Param('id') id:string, @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto) {
-        return this.coffeesServices.update(id,updateCoffeeDto)
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id:string) {
-        return this.coffeesServices.remove(id)
-    }
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.coffeesServices.remove(id);
+  }
 }
